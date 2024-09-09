4d14114be73aab1721357a6b0a3a779f75a919d0
Arshiya <Arshiyanegahdar7@gmail.com>
2024-09-09 16:47:05 +0330

    arraySimilarity is done.

--- 06-is Tow Array similar/arraySimilarity.js
+++ 06-is Tow Array similar/arraySimilarity.js
@@ -1,1 +1,9 @@
 // write your func here
+const array1 = [1, 2, 3];
+const array2 = [1, 2, 3];
+
+if (areArraysEqual(array1, array2)) {
+    console.log("آرایه‌ها برابر هستند.");
+} else {
+    console.log("آرایه‌ها برابر نیستند.");
+}

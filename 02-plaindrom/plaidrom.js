ba56e6fe89d0c0118a5f0bf493e50ad0dae457f0
Arshiya <Arshiyanegahdar7@gmail.com>
2024-09-09 16:29:12 +0330

    plaidrom done.

--- 02-plaindrom/plaidrom.js
+++ 02-plaindrom/plaidrom.js
@@ -1,1 +1,12 @@
 // write your func here
+function isPalindrome(str) {
+    const cleanStr = str.replace(/\s+/g, '').toLowerCase();
+    
+    const reversedStr = cleanStr.split('').reverse().join('');
+    
+    return cleanStr === reversedStr;
+  }
+  
+  console.log(isPalindrome("madam"));          
+  console.log(isPalindrome("سلام"));           
+  console.log(isPalindrome("نیما تیمان"));     
